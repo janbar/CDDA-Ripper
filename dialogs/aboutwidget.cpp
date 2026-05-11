ad6c60697364f0faf3e7b7b80361f7e8a56821c1
janbar <jlbarriere68@gmail.com>
2026-05-11 23:34:14 +0200

    remove unused args

--- dialogs/aboutwidget.cpp
+++ dialogs/aboutwidget.cpp
@@ -15,7 +15,7 @@ AboutWidget::AboutWidget(QWidget *parent)
           "<html>An easy to use audio CD ripping application.<br>"
           "<br><b>Version %1</b>, (c) Jean-Luc Barrière<br>"
           "<br>CDDA-Ripper was forked from Audex-0.95, and reworked outside of the KDE frameworks.</html>")
-          .arg(CDDARIPPER_VERSION).arg(QT_VERSION_MAJOR).arg(QT_VERSION_MINOR).arg(QT_VERSION_PATCH));
+          .arg(CDDARIPPER_VERSION));
 
   connect(ui.buttonBox, SIGNAL(clicked(QAbstractButton*)), this, SLOT(close()));
 }
