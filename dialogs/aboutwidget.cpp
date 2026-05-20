141eaf838f1075200a2a3ab356a6a1d5f51334bb
janbar <jlbarriere68@gmail.com>
2026-05-20 16:25:09 +0200

    fix about page for big font

--- dialogs/aboutwidget.cpp
+++ dialogs/aboutwidget.cpp
@@ -14,12 +14,17 @@ AboutWidget::AboutWidget(QWidget *parent)
   connect(buttonBox, SIGNAL(rejected()), this, SLOT(close()));
 
   ui.label_icon1->setPixmap(QPixmap(":/icons/cddaripper-64x64.png").scaledToWidth(48, Qt::SmoothTransformation));
-  ui.label_icon2->setPixmap(QPixmap(":/icons/musicbrainz.png").scaledToWidth(48, Qt::SmoothTransformation));
-  ui.label_icon3->setPixmap(QPixmap(":/icons/deezer.png").scaledToWidth(48, Qt::SmoothTransformation));
+  ui.label_icon2->setPixmap(QPixmap(":/icons/paranoia.png").scaledToWidth(48, Qt::SmoothTransformation));
+  ui.label_icon3->setPixmap(QPixmap(":/icons/musicbrainz.png").scaledToWidth(48, Qt::SmoothTransformation));
+  ui.label_icon4->setPixmap(QPixmap(":/icons/deezer.png").scaledToWidth(48, Qt::SmoothTransformation));
 
   ui.text_about1->setText(tr("<html>An easy to use audio CD ripping application."
           "<br><br><b>Version %1</b>, (c) Jean-Luc Barrière"
           "<br><br>CDDA-Ripper was forked from Audex-0.95, and reworked outside of the KDE frameworks."
+          " MusicBrainz and Deezer are used to fetch metadata and cover arts."
+          " CDParanoia is the main backend to extract audio track."
+          "<br>Note that you should install your chosen encoders separately."
+          " FLAC, LAME, FAAC, OGG, and OPUS encoders are currently supported."
           "<br><br><b>Credits&thinsp;:</b>"
           "<br>Audex-0.95 (Marco Nelles)"
           "<br>Solid (kde.org)"
